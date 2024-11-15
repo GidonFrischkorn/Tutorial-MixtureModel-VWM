1167ecb4dd549fcc3ff593c8c6dfa0f8bfc8d9c2
Ven Popov <vencislav.popov@gmail.com>
2024-11-15 16:00:16 +0100

    Replace the mixtur2p script with the latest on the server
    - it used the queue package for massive parallelization. The repo script was outdated

--- scripts/parameter_recovery/mixture2p_mixtur.R
+++ scripts/parameter_recovery/mixture2p_mixtur.R
@@ -1,36 +1,73 @@
-library(bmm)
-library(mixtur)
-library(here)
+library(pacman)
+p_load(here)
+p_load_current_gh(
+  "venpopov/mixtur@rescale_parameters", "venpopov/bmm",
+  "djnavarro/queue"
+)
 
-fit_m2_ml <- function(N_obs) {
-  pars <- expand.grid(kappa = seq(1, 16, 0.5),
-                      pmem = seq(0.6, 1, 0.025))
-  pars$id <- 1:nrow(pars)
-  pars <- pars[,c('id','pmem','kappa')]
-  N_subj <- nrow(pars)
-  
+# number of cores to use for the simulation
+cores <- 30
+if (cores > future::availableCores()) cores <- future::availableCores() - 1
+
+# generates data from the mixture2p model, and fits it via the mixtur package
+fit_m2_ml <- function(n_obs, pars) {
+  pars$id <- seq_len(nrow(pars))
+  pars <- pars[, c("id", "pmem", "kappa")]
+  N_subj <- nrow(pars) # nolint: object_name_linter.
+
   data <- list()
   for (i in 1:N_subj) {
-    data[[i]] <- rmixture2p(N_obs, mu = 0, pMem = pars$pmem[i], kappa = pars$kappa[i])
+    data[[i]] <- bmm::rmixture2p(
+      n_obs,
+      mu = 0,
+      pMem = pars$pmem[i],
+      kappa = pars$kappa[i]
+    )
   }
-  
-  data <- data.frame(id = rep(1:N_subj, each = N_obs), 
-                     y = unlist(data),
-                     target = rep(0, N_subj * N_obs))
-  
-  par_ml <- fit_mixtur(data, model = "2_component", unit = "radians", response_var = "y")
-  names(par_ml) <- c('id','kappa','pmem','pguess')
-  par_ml <- par_ml[,c('id','pmem','kappa')]
+
+  data <- data.frame(
+    id = rep(1:N_subj, each = n_obs),
+    y = unlist(data),
+    target = rep(0, N_subj * n_obs)
+  )
+
+  par_ml <- mixtur::fit_mixtur(
+    data,
+    model = "2_component",
+    unit = "radians",
+    response_var = "y"
+  )
+
+  names(par_ml) <- c("id", "kappa", "pmem", "pguess")
+  par_ml <- par_ml[, c("id", "pmem", "kappa")]
+  par_ml$Nobs <- n_obs
   par_ml
 }
 
+# store results
+file <- here("output/mixture2p_mixtur_fullgrid_recovery_unconstrained.RData")
 
-N_obs <- c(20, 50, 200, 500)
+if (!file.exists(file)) {
+  # setup simulation details
+  pars <- expand.grid(
+    kappa = seq(1, 16, 0.5),
+    pmem = seq(0.1, 1, 0.025)
+  )
+  n_obs <- c(20, 50, 100, 200, 500) # number of observations per design cell
+  rep <- 1000 # number of replications of each simulation
 
-fits20 <- replicate(200, fit_m2_ml(N_obs = 20))
-fits50 <- replicate(200, fit_m2_ml(N_obs = 50))
-fits100 <- replicate(200, fit_m2_ml(N_obs = 100))
-fits200 <- replicate(200, fit_m2_ml(N_obs = 200))
-fits500 <- replicate(200, fit_m2_ml(N_obs = 500))
+  # setup parallel tasks in external r session via the queue package
+  queue <- Queue$new(workers = cores)
+  for (N in n_obs) {
+    for (r in 1:rep) {
+      queue$add(fit_m2_ml, args = list(n_obs = N, pars = pars))
+    }
+  }
 
-save(fits20, fits50, fits100, fits200, fits500, file = here('output/mixture2p_mixtur_grid_recovery.RData'))
+  out <- queue$run(message = "verbose")
+  res <- out$result
+
+  dir <- dirname(file)
+  if (!dir.exists(dir)) dir.create(dir)
+  save(res, pars, file = file)
+}
